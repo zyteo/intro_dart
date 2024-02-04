c5fca98bcaab3fd8c20a5d71f3dbbd386d72763e
Stabilo <zteo015@e.ntu.edu.sg>
2024-02-04 20:34:05 +0800

    first commit

---
+++ basics.dart
@@ -0,0 +1,19 @@
+void main() {
+    var name = "mario";
+    print(name);
+    const age = 30;
+    print(age);
+    const isOpen = true;
+    print(isOpen);
+
+    print(age + 10);
+    print(age - 10);
+    print(age * 10);
+    print(age / 10);
+    print(age % 7);
+
+    print("my name is " + name);
+    print("my name is $name");
+    print("my name is $name and I am $age years old")
+
+}
